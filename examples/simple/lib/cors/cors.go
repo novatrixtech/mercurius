b7c6fc4efe4df56a39f221c1c682e6b27dbd0aa7
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-07-16 20:46:16 -0400

    add patch on cors func

--- examples/simple/lib/cors/cors.go
+++ examples/simple/lib/cors/cors.go
@@ -8,7 +8,7 @@ import (
 func Cors() macaron.Handler {
 	return func(ctx *context.Context) {
 		ctx.Resp.Header().Set("Access-Control-Allow-Origin", "*")
-		ctx.Resp.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		ctx.Resp.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
 		ctx.Resp.Header().Set("Access-Control-Allow-Headers", "*")
 		ctx.Resp.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Next()
